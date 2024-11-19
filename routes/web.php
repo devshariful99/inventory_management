50b33a4b526c749a107ca169f9f31fd268bf8ca0
devshariful99 <devshariful99@gmail.com>
2024-11-19 12:14:15 +0600

    Master template setup--done

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Auth::routes();
+Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function () {
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('admin.dashboard');
+
+    // Admin Management
+    // Route::group(['as' => 'am.', 'prefix' => 'admin-management'], function () {});
+
+});
